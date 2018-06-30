f56054ebad6f4988bb023da73c6c99bb56f59cc6
crucialjun <nich.otieno@gmail.com>
2018-06-30 23:13:31 +0300

    update

--- app/src/main/java/com/example/android/triviaapp/MainActivity.java
+++ app/src/main/java/com/example/android/triviaapp/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +17,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void submitAnswer(View view){
-        RadioGroup radioGroup = findViewById(R.id.question1);
+    public void submitAnswer(View view) {
+        int score = 0;
+
+        CheckBox checkBox2a = findViewById(R.id.question2a);
+        CheckBox checkBox2c = findViewById(R.id.question2c);
+
+        RadioButton  radioButton1D = findViewById(R.id.question1d);
+        if (radioButton1D.isChecked()){
+            score = score + 1;
+        }
+
+        if (checkBox2a.isChecked() && checkBox2c.isChecked()){
+            score = score + 1;
+        }
+
+
+    }
 
 }
